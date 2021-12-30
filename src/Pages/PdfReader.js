88f1fd5a2e7ccc2e9675bc6076891ceab4b23d79
unknown <jagadyudhaawali@gmail.com>
2021-12-30 23:22:51 +0700

    feat: add Dragable image

--- src/Pages/PdfReader.js
+++ src/Pages/PdfReader.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import Pdf from 'react-native-pdf';
 import { windowWidth } from '../Utils/Dimension';
 import { fontSizer } from '../Utils/Font';
+import { DragResizeBlock, DragResizeContainer } from 'react-native-drag-resize';
 
 const PdfReader = ({ route, navigation }) => {
   const { pdfurl } = route.params;
   const [pageSize, setPageSize] = React.useState(null);
   const [signatureBase64, setSignatureBase64] = React.useState(null);
+  const [isDragable, setIsDragable] = React.useState(false);
+  const [location, setLocation] = React.useState(null);
+  const [signatureSize, setSignatureSize] = React.useState(null);
 
-  console.log(signatureBase64);
-  console.log(pageSize);
   return (
     <View style={styles.container}>
       <Pdf
@@ -38,8 +40,18 @@ const PdfReader = ({ route, navigation }) => {
         onPressLink={(uri) => {
           console.log(`Link presse: ${uri}`);
         }}
-        onPageSingleTap={(page, x, y) => {
-          this.handleSingleTap(page, x, y);
+        // onPageSingleTap={(page, x, y) => {
+        //   this.handleSingleTap(page, x, y);
+        // }}
+
+        onTouchStart={(e) => {
+          if (signatureBase64 && !isDragable) {
+            setIsDragable(true);
+            setLocation({
+              cxValue: e.nativeEvent.locationX,
+              cyValue: e.nativeEvent.locationY,
+            });
+          }
         }}
         style={styles.pdf}
       />
@@ -67,6 +79,69 @@ const PdfReader = ({ route, navigation }) => {
           Tanda Tangan
         </Text>
       </TouchableOpacity>
+
+      {isDragable ? (
+        <DragResizeBlock
+          isResizable
+          isDraggable
+          x={location.cxValue}
+          y={location.cyValue}
+          w={50}
+          h={50}
+          // onDragEnd={this._dragRelease}
+          //onResizeEnd={(e) => console.log(e)}
+          // onResizeEnd={this._resizeRelease}
+          connectors={['tl', 'tr', 'br', 'bl', 'c']}
+        >
+          <View
+            onLayout={(e) =>
+              setSignatureSize({
+                width: e.nativeEvent.layout.width,
+                height: e.nativeEvent.layout.height,
+              })
+            }
+            style={{
+              width: '100%',
+              height: '100%',
+              alignContent: 'center',
+              alignItems: 'center',
+              borderColor: '#000',
+              borderWidth: 1,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                zIndex: 10,
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                backgroundColor: '#FFFFFF',
+              }}
+            ></View>
+            <Image
+              source={{
+                uri: `data:image/png;base64,${signatureBase64}`,
+              }}
+              style={{ aspectRatio: 4 / 3, width: '100%', height: '100%' }}
+              resizeMode={'cover'}
+            />
+          </View>
+          <Text style={{ marginTop: 20 }}>Klik Untuk Submit</Text>
+          {signatureSize && (
+            <Image
+              source={{
+                uri: `data:image/png;base64,${signatureBase64}`,
+              }}
+              style={{
+                width: signatureSize.width,
+                height: signatureSize.height,
+              }}
+            />
+          )}
+        </DragResizeBlock>
+      ) : null}
     </View>
   );
 };
